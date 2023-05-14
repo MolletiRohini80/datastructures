04079bc7f56489d2a0a9676d61b23c84e5e73c5f
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-05-14 20:25:57 +0530

    Add files via upload

---
+++ logical operators.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("enter a,b value");
+	scanf("%d%d",&a,&b);
+	if(a>b&&a>!b);
+	{
+	printf("a is greater than b and not greater than to b\n");
+	}
+	if(a=b||a!=b);
+	{
+	printf("a is equal to b or not equal to b\n");
+	}
+}
+
