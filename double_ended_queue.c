f30236e7a486c464f8d17f31ef8a96580bf895a8
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-05-14 20:23:07 +0530

    Add files via upload

---
+++ double_ended_queue.c
@@ -0,0 +1,177 @@
+#include<stdio.h>
+int queue[100],size,rear=-1,front=-1;
+void enque_rear(int val)
+{
+	if(front==0 && rear==size-1)
+	{
+		printf("Queue is full\n");
+	}
+	else if(rear==front-1)
+	{
+		printf("Queue is full\n");
+	}
+	else if(rear==-1 && front==-1)
+	{
+		rear=0;
+		front=0;
+		queue[rear]=val;
+	}
+	else
+	{
+		rear=(rear+1)%size;
+		queue[rear]=val;
+	}
+}
+void display()
+{
+	int i=front;
+	if(rear==-1 && front==-1)
+	{
+		printf("Queue is empty\n");
+		return;
+	}
+	while(i!=rear)
+	{
+		printf("%d",queue[i]);
+		i=(i+1)%size;
+	}
+	printf("%d\n",queue[i]);
+}
+int deque_front()
+{
+	int val;
+	if(front==-1 && rear==-1)
+	{
+		return -1;
+	}
+	else if(front==rear)
+	{
+		val=queue[front];
+		front=-1;
+		rear=-1;
+		return val;
+	}
+	else
+	{
+		val=queue[front];
+		front=(front+1)%size;
+		return val;
+	}
+}
+void enque_front(int val)
+{
+   if(front==0 && rear==size-1)
+   {
+   	printf("Queue is full\n");
+	}
+	else if(rear=front-1)
+	{
+		printf("Queue is full\n");
+	}
+	else if(front==-1 && rear==-1)
+	{
+		rear=0;
+		front=0;
+		queue[front]=val;
+	}
+	else if(front==0)
+	{
+		front=size-1;
+		queue[front]=val;
+	}	
+	else
+	{
+		front--;
+		queue[front]=val;
+	}
+}
+int deque_rear()
+{
+	int val;
+	if(front==0 && rear==size-1)
+   {
+   	printf("Queue is full\n");
+	}
+	else if(rear=front-1)
+	{
+		printf("Queue is full\n");
+	}
+	else if(front==-1 && rear==-1)
+	{
+		return-1;
+	}
+	else if(front==rear)
+	{
+		val=queue[rear];
+		rear=-1;
+		front=-1;
+		return val;
+	}
+	else if(rear==0)
+	{
+		val=queue[rear];
+		rear=size-1;
+		return val;
+	}
+	else
+	{
+		rear--;
+		rear=size-1;
+	}
+}
+int main()
+{
+	int ch,val;
+	scanf("%d",&size);
+	while(1)
+	{
+		printf("1.enque at rear \n 2.deque at front\n 3.display \n 4.enque at front \n5.deque at rear \n 6.exit:");
+		scanf("%d",&ch);
+		if(ch==1)
+		{
+			//enque at rear
+			scanf("%d",&val);
+			enque_rear(val);
+		}
+		else if(ch==2)
+		{
+			//deque at front'
+			val=deque_front();
+			if(val==-1)
+			{
+				printf("Queue is empty\n");
+			}
+			else
+			{
+				printf("%d\n",val);
+			}
+		}
+		else if(ch==3)
+		{
+			//display
+			display();
+		}
+		else if(ch==4)
+		{
+			scanf("%d",&val);
+			enque_front(val);
+		}
+		else if(ch==5)
+		{
+			scanf("%d",&val);
+			val=deque_rear();
+			if(val==-1)
+			{
+				printf("Queue is empty\n");
+			}
+			else
+			{
+				printf("%d\n",val);
+			}
+		}
+		else 
+		{
+			break;
+		}
+	}
+}
